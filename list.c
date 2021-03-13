2e1bc8ba2ff135c64b23a31e17419753d4d283f2
luismdsleite <79144290+luismdsleite@users.noreply.github.com>
2021-03-13 21:49:11 +0000

    Update list.c

--- list.c
+++ list.c
@@ -1,7 +1,8 @@
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "task.h"
+#include "task.c"
 
 typedef struct lnode *List;
 typedef struct lnode
@@ -11,8 +12,9 @@ typedef struct lnode
 } List_node;
 
 
-List create_list(void)
+List create_list(long* counter)
 {
+    *counter = 0;
     List aux;
     aux = (List)malloc(sizeof(List_node));
     if (aux != NULL)
@@ -20,13 +22,16 @@ List create_list(void)
         aux->task = (Task *) malloc(sizeof(Task));
         aux->task->id = 0;
         aux->task->made_in = 0;
+        aux->task->date = 0;
+        char* person = create_string("");
+        if(person != NULL) aux->task->person = person;
         aux->next = NULL;
     }
     return aux;
 }
 
 
-void search_list(List list, Task* to_find , List *prev, List *cur)
+void search_list_made_in(List list, Task* to_find , List *prev, List *cur)
 {
     *prev = list;
     *cur = list->next;
@@ -43,17 +48,66 @@ void search_list(List list, Task* to_find , List *prev, List *cur)
         *cur = NULL; /* Se elemento não encontrado*/
 }
 
+void search_list_date(List list, Task* to_find , List *prev, List *cur)
+{
+    *prev = list;
+    *cur = list->next;
+    time_t date = to_find->date;
+    while ( (*cur) != NULL &&  
+            ( (*cur)->task->date < date ||
+            ((*cur)->task->date == date &&  (*cur)->task->id < to_find -> id) )
+        )
+    {
+        *prev = *cur;
+        *cur = (*cur)->next;
+    }
+    if ((*cur) != NULL && (*cur)->task->date != date)
+        *cur = NULL; /* Se elemento não encontrado*/
+}
 
-void insere_list(List list, Task *it) /* ordenado */
+void search_list_person(List list, Task* to_find , List *prev, List *cur)
+{
+    *prev = list;
+    *cur = list->next;
+    char* person = to_find->person;
+    while ( (*cur) != NULL &&  
+            ( strcmp((*cur)->task->person,person) < 0 ||
+            ( strcmp((*cur)->task->person, person) == 0 &&  (*cur)->task->id < to_find -> id ) )
+        )
+    {
+        *prev = *cur;
+        *cur = (*cur)->next;
+    }
+    if ((*cur) != NULL && strcmp((*cur)->task->person, person) != 0)
+        *cur = NULL; /* Se elemento não encontrado*/
+}
+
+void search_mode(List list, Task* it , List *prev, List *cur, int mode){
+    switch (mode)
+        {
+        case 1:
+            search_list_date(list, it, prev, cur);
+            break;
+
+        case 2:
+            search_list_person(list, it, prev, cur);
+            break;
+
+        default:
+            search_list_made_in(list, it, prev, cur);
+            break;
+        }
+}
+
+void insert_list(List list, Task *it, int mode) /* ordenado */
 {
     List no;
     List prev, inutil;
     no = (List)malloc(sizeof(List_node));
     if (no != NULL)
     {
-
         no->task = it;
-        search_list(list, it, &prev, &inutil);
+        search_mode(list, it, &prev, &inutil, mode); 
         no->next = prev->next;
         prev->next = no;
     }
@@ -64,7 +118,7 @@ void print_list(List list)
     List l = list->next; /* Salta o header */
     while (l)
     {
-        printf("|%d|%ld ", l->task->id, l->task->made_in);
+        printf("|%d|%s|%ld ", l->task->id, l->task->person,l->task->date);
         l = l->next;
     }
 }
@@ -73,41 +127,13 @@ List find_list(List list, Task* it){
     List prev;
     List cur;
 
-    search_list(list, it, &prev, &cur);
+    search_list_made_in(list, it, &prev, &cur);
 
     return (cur);
 }
 
 
 
-int main(int argc, char const *argv[])
-{   
-    time_t t1 = time(NULL);
-    printf("%s",ctime(&t1));
-
-    List list = create_list();
-    Task* s1 = (Task*) malloc(sizeof(Task));
-    s1->made_in = t1;
-    s1->id = 1;
-
-    Task* s2 = (Task*) malloc(sizeof(Task));
-    s2->made_in = t1+20; 
-    s2->id = 2;
-
-    Task* s3 = (Task*) malloc(sizeof(Task));
-    s3->made_in = t1+20; 
-    s3->id = 3;
-
-    insere_list(list, s1);
-    insere_list(list, s2);
-    insere_list(list, s3);
-    print_list(list);
-    List res = find_list(list,s2);
-    printf("\n");
-    printf("%d\n", res->task->id);
-    print_list(res);
-    return 0;
-}
 
 
 
