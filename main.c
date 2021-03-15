fdb3f363cdfe51a82d84606a0fc6cbcde2d7cc53
luismdsleite <79144290+luismdsleite@users.noreply.github.com>
2021-03-15 12:58:29 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "list.c"
 #include "task.c"
 
-/*so estava a usar isto para testar*/
+/*so estou a usar isto para testar*/
 int main(int argc, char const *argv[])
 {   
     time_t t1 = time(NULL);
     //printf("%s",ctime(&t1));
 
-    long counter=0;
+    long counter=0; // Usado para criar o id
     List list = create_list();
     time_t t2 = t1 + 2222;
     Task* s1 = create_task(&counter, "info1", &t2, "c");
@@ -17,11 +17,13 @@ int main(int argc, char const *argv[])
     Task* s3 = create_task(&counter, "info3",&t2, "a");
 
     int mode=1;
-    insert_list(list, s1, mode);
     insert_list(list, s2, mode);
     insert_list(list, s3, mode);
+    insert_list(list, s1, mode);
+    
+    
     print_list(list);
-    List res = find_list(list,s2);
+    List res = find_list(list,s2->id);
     printf("\n");
     printf("id=%ld\n", res->task->id);
     printf("counter = %ld\n",counter);
