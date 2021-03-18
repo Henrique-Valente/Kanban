6701586c4c48788616cf85cc52b3aaf733b9fb6e
Henrique-Valente <64140129+Henrique-Valente@users.noreply.github.com>
2021-03-18 15:30:22 +0000

    Update kanban.c

--- kanban.c
+++ kanban.c
@@ -5,9 +5,18 @@ Kanban* create_kanban()
 {
     Kanban* output = (Kanban*) malloc(sizeof(Kanban));
     output->counter = 0;
-    //Nao Implementado ainda
-    //output->to_do = create_to_do();
     output->doing = create_list();
     output->done = create_list();
     return output;
+}
+
+List *create_to_do(void)
+{
+    List* priority = malloc(sizeof(List)*10);
+    for (int i = 0; i < 10; i++)
+    {
+        priority[i] = malloc(sizeof(List));
+        priority[i] = create_list();
+    }
+    return priority;
 }
