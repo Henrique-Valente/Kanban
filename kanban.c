fee7f67c673d62c5a47813bd43db2924a8f37c7d
Henrique-Valente <64140129+Henrique-Valente@users.noreply.github.com>
2021-03-18 15:51:14 +0000

    Update kanban.c

--- kanban.c
+++ kanban.c
@@ -5,7 +5,6 @@ List *create_to_do(void)
     List* priority = malloc(sizeof(List)*10);
     for (int i = 0; i < 10; i++)
     {
-        priority[i] = malloc(sizeof(List));
         priority[i] = create_list();
     }
     return priority;
