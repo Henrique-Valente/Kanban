298cec8a4c794775b07d1cf1ae99b778aff1b5a1
luismdsleite <luismdsleite@gmail.com>
2021-03-18 18:14:59 +0000

    restaurando o create_kanban()

--- kanban.c
+++ kanban.c
@@ -1,12 +1,10 @@
 #include "kanban.h"
 
-List *create_to_do(void)
+Kanban* create_kanban()
 {
-    List* priority = malloc(sizeof(List)*10);
-    for (int i = 0; i < 10; i++)
-    {
-        priority[i] = create_list();
-    }
-    return priority;
+    Kanban* output = (Kanban*) malloc(sizeof(Kanban));
+    output->counter = 0;
+    output->doing = create_list();
+    output->done = create_list();
+    return output;
 }
-
